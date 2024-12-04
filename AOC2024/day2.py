7f019439e925b62a49761b0c4cd864eade894be9
Olivia Baker <olivia@usbakers.com>
2024-12-04 21:24:37 +0000

    Completed day 2. Changes to the python code file.

--- AOC2024/day2.py
+++ AOC2024/day2.py
@@ -2,6 +2,9 @@
 
 
 
+from tabnanny import check
+
+
 with open('d2.txt', 'r+') as f:
     x = f.readlines()
 
@@ -11,16 +14,9 @@ lis = []
 safeCnt = 0
 
 
-
-for i in x:
-    lis.append(i.split(' '))
-
-
-
-for i in lis:
-    safe = True
-    add = 0
-
+def CheckSafe(i):
+    safe=True
+    add=0
     for a in range(len(i)):
         try:
             int(i[a+1])
@@ -37,6 +33,20 @@ for i in lis:
 
     if add + 1 != len(i) and add != 0:
         safe = False
+
+    return safe
+
+
+
+
+for i in x:
+    lis.append(i.split(' '))
+
+
+
+for i in lis:
+    safe = CheckSafe(i)
+ 
 
     if safe == True:
         safeCnt += 1
@@ -66,105 +76,23 @@ safeCnt = 0
 for i in x:
     lis.append(i.split(' '))
     
-        
-
 
 
 for i in lis:
-    safe = True
-    add = 0
+    safe = CheckSafe(i)
+ 
 
-    for a in range(len(i)):
-        try:
-            int(i[a+1])
-        except IndexError:
-            pass
-        else:
-            b = int(i[a]) - int(i[a+1])
-
-            if b > 3 or b < -3 or b == 0:
-                safe = False
-
-            if b > 0:
-                add += 1
-
-    if add + 1 != len(i) and add != 0:
-        safe = False
 
     if safe == True:
         safeCnt += 1
-        pass
     else:
-        failLis.append(i)
-
-
-
-lis.clear()
-       
-for i in failLis:
-    temp = []
-    for a in i:
-        temp.append(int(a))
-    lis.append(temp)
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-for i in lis:
-    fails = 0
-    iCopy = i.copy()
-    for a in i:
-        safe = True
-        aPos = i.index(a)
-        
-        iCopy.remove(a)
-
-        for j in range(len(iCopy)):
-
-            try:
-                iCopy[j+1]
-            except IndexError:
-                pass
-            else:
-                b = iCopy[j] - iCopy[j+1]
-
-                if b > 3 or b < -3 or b == 0:
-                    safe = False
-
-                if b > 0:
-                    add += 1
-
-            if add + 1 != len(iCopy) and add != 0:
-                safe = False
-
-        if safe == False:
-            fails += 1
-            
-
-
-
-
-        iCopy.insert(aPos, a)
-        #print(aPt)
-
-
-    if fails < 2:
-        safeCnt += 1
-
-
-
+        for a in range(len(i)):
+            newi=i.copy()
+            newi.pop(a)
+            safe = CheckSafe(newi)
+            if safe:
+                safeCnt += 1
+                break
 
 
 
